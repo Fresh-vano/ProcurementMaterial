1a76ea937620906ed954baa39d0738b719424a05
LEATID <92569798+LEATID@users.noreply.github.com>
2024-05-20 15:21:56 +0500

    Update ModelDok_SF.cs
    new

--- ProcurementMaterialAPI/ProcurementMaterialAPI/ModelDB/ModelDok_SF.cs
+++ ProcurementMaterialAPI/ProcurementMaterialAPI/ModelDB/ModelDok_SF.cs
@@ -1,6 +1,19 @@
-﻿namespace ProcurementMaterialAPI.ModelDB
+﻿using Microsoft.Identity.Client;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProcurementMaterialAPI.ModelDB
 {
     public class ModelDok_SF
     {
+        public int buisnes_number {  get; set; }
+        public int buisnes_consignee { get; set; }
+        public int fact_number { get; set; }
+        public int fact_pos { get; set; }
+        public string material { get; set; }
+        public string material_name {  get; set; }
+        public DateOnly date_budat { get; set; }
+        public string material_grpup {  get; set; }
+        public char  EI {  get; set; }
+
     }
 }
