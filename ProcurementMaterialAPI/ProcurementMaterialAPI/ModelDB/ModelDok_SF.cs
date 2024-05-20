446099437cd4581cdb5658b485ba73e850f74e6c
LEATID <92569798+LEATID@users.noreply.github.com>
2024-05-20 15:36:29 +0500

    Update ModelDok_SF.cs
    newaa

--- ProcurementMaterialAPI/ProcurementMaterialAPI/ModelDB/ModelDok_SF.cs
+++ ProcurementMaterialAPI/ProcurementMaterialAPI/ModelDB/ModelDok_SF.cs
@@ -6,14 +6,31 @@ namespace ProcurementMaterialAPI.ModelDB
     public class ModelDok_SF
     {
         public int buisnes_number {  get; set; }
+
         public int buisnes_consignee { get; set; }
+
         public int fact_number { get; set; }
+
         public int fact_pos { get; set; }
+
         public string material { get; set; }
+
         public string material_name {  get; set; }
+
+        public string material_type { get; set; }
+
         public DateOnly date_budat { get; set; }
-        public string material_grpup {  get; set; }
+
+        public string material_group {  get; set; }
+
         public char  EI {  get; set; }
+
+        public string INN { get; set; }
+
+        public decimal quan {  get; set; }
+
+        public decimal cost { get; set; }
+
 
     }
 }
